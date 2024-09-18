18d5f579031009d7a7f82a29de91d69e69c5503e
onicarn <onicarn@XUBUNTU.myguest.virtualbox.org>
2024-09-18 23:53:21 +0200

    SEgundo repositorio con el lugar favorito

--- Prrueba.java
+++ Prrueba.java
@@ -1,5 +1,7 @@
 public class Prrueba {
 	public static void main(String[]args){
 	System.out.println("MI deporte favorito es el futbol");
+	System.out.println("MI lugar favorito es el Siam Park");
+
    }
  } 
